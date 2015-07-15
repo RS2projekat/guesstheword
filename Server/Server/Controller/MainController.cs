29a91e78519b3da86ce2c47623c7cbc33868e4e1
bokisha <bojants91@gmail.com>
2015-07-16 00:51:37 +0200

    Added start mvc structure

---
+++ Server/Server/Controller/MainController.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Server.Controller
+{
+    public class MainController
+    {
+        public MainWindow View { get; set; }
+
+        private void AttachEvents()
+        {
+            View.TextBoxPort.PreviewTextInput += NumberValidationTextBox;
+            View.ButtonServerStart.Click += ButtonServerStartOnClick;
+        }
+
+        // -----------------------------------------------------------------------------------
+        private void ButtonServerStartOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            View.ButtonServerStart.Content = "Stop";
+        }
+
+        // -----------------------------------------------------------------------------------
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            if (!char.IsDigit(e.Text, e.Text.Length - 1))
+            {
+                e.Handled = true;
+
+            }
+        }
+
+        // -----------------------------------------------------------------------------------
+        internal void Init(MainWindow main)
+        {
+            View = main;
+
+            AttachEvents();
+            DoBinding();
+        }
+
+        // -----------------------------------------------------------------------------------
+        private void DoBinding()
+        {
+            
+        }
+    }
+}
---
+++ Server/Server/Model/Server.cs
@@ -0,0 +1,13 @@
+﻿namespace Common
+{
+    public class Server
+    {
+        private int _port;
+
+
+        public int Port
+        {
+            get { return _port; }
+        }
+    }
+}
