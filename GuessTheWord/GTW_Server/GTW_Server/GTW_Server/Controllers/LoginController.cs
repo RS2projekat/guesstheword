5db80105b9c100196c7946e5f342ff5816975a5a
missuchiha93@gmail.com <Sasukeuchiha13>
2015-08-22 19:28:22 +0200

    "Added LoginControler in backend."

---
+++ GuessTheWord/GTW_Server/GTW_Server/GTW_Server/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XSockets.Core.XSocket;
+using XSockets.Core.XSocket.Helpers;
+using XSockets.Core.Common.Socket.Event.Interface;
+using XSockets.Plugin.Framework.Attributes;
+using GTW_Server.Models;
+
+namespace GTW_Server.Controllers
+{
+    [XSocketMetadata("login")]
+    public class LoginController : XSocketController
+    {
+        /// <param name="signin"></param>
+        public void SignIn(LoginUserModel user)
+        {
+            if (user.Username.Equals("admin") && user.Password.Equals("admin"))
+            {
+                this.Invoke(user.Username, "loggedin");
+            }
+            else
+            {
+                this.Invoke("notloggedin");
+            }
+        }
+
+        /// <param name="signup"></param>
+        public void SignUp(LoginUserModel user)
+        {
+            if (user.Password.Equals(user.ConfirmPassword) == false)
+                this.Invoke("noequalpass");
+            else
+                this.Invoke("signedup");
+        }
+    }
+}
---
+++ GuessTheWord/GTW_Server/GTW_Server/GTW_Server/Models/LoginUserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GTW_Server.Models
+{
+    public class LoginUserModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+    }
+}
